f969dbfabca748d200e6bd075e9202c85a4d7abd
withdrw <finesse@usa.com>
2024-02-01 16:41:16 -0600

    Phase 2

--- problems/number-fun.js
+++ problems/number-fun.js
@@ -1,11 +1,19 @@
 function returnsThree() {
-  // Your code here 
+  // Your code here
+  return 3;
 }
 
 function reciprocal(n) {
-  // Your code here 
+  // Your code here
+  if (n < 1) {
+   return ('RangeError: Please enter a number greater than 1')
+  }
+  if (n > 1000000) {
+    return ('RangeError: Please enter a number less than 1000001')
+  }
+  return 1 / n
 }
-
+console.log(reciprocal(.99))
 module.exports = {
   returnsThree,
   reciprocal
--- test/number-fun-spec.js
+++ test/number-fun-spec.js
@@ -1,1 +1,36 @@
-// Your code here 
+// Your code here
+const chai = require('chai')
+const { reciprocal, returnsThree } = require('../problems/number-fun')
+const expect = chai.expect
+
+
+describe('returnsThree()', () => {
+    it('should return the number 3', () => {
+        const input = returnsThree();
+        const expected = 3;
+        expect(input).to.equal(expected)
+    })
+})
+
+describe('reciprocal(num)',() => {
+    context('valid arguments', () => {
+        it('should return the reciprocal of the number', () => {
+            const input1 = reciprocal(2);
+            const input2 = reciprocal(5);
+            const expected1 = 0.5;
+            const expected2 = 0.2;
+            expect(input1).to.equal(expected1)
+            expect(input2).to.equal(expected2)
+        })
+    })
+    context('invalid arguments', () => {
+        it('should return a RangeError', () => {
+            const input1 = reciprocal(0)
+            const input2 = reciprocal(1000001)
+            const expected1 = 'RangeError: Please enter a number greater than 1'
+            const expected2 = 'RangeError: Please enter a number less than 1000001'
+            expect(input1).to.equal(expected1)
+            expect(input2).to.equal(expected2)
+        })
+    })
+} )
