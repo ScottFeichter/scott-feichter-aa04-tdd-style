dbba02d1942166dd90ce39b2f9f7b6831e0ce629
Scott Feichter <scottfeichter@gmail.com>
2024-02-01 14:00:15 -0800

    phase 01 finished

--- test/reverse-string-spec.js
+++ test/reverse-string-spec.js
@@ -1,1 +1,11 @@
-// Your code here 
+const chai = require(`chai`);
+const expect = chai.expect;
+const reverseString = require(`../problems/reverse-string`);
+
+describe("reverseString(string)", () => {
+  it(`should return the reversed string`, () => {
+    const input = reverseString("fun");
+    const expected = "nuf";
+    expect(input).to.eql(expected);
+  })
+})
